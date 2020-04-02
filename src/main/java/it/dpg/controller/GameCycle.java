0cb6ba5e404301357f1c581d7f6233f114a77cb0
Davide <davide.freddi3@studio.unibo.it>
2020-04-02 18:57:01 +0200

    added interface methods and documentation

--- src/main/java/it/dpg/controller/GameCycle.java
+++ src/main/java/it/dpg/controller/GameCycle.java
@@ -1,4 +1,26 @@
 package it.dpg.controller;
 
+import java.util.Set;
+
+/**
+ * coordinates the elements of the game
+ */
 public interface GameCycle {
+    /**
+     * start the game cycle with a new thread
+     * @param nCharacters number of characters in the game
+     * @param playerNames names of the players, witch have to be less than the number of characters
+     * @throws IllegalArgumentException id playerNames.size() > nCharacters
+     */
+    void startGameCycle(int nCharacters, Set<String> playerNames) throws IllegalArgumentException;
+
+    /**
+     * method used to notify the game cycle the dice has been thrown by a player
+     */
+    void signalDiceThrown();
+
+    /**
+     * method used to notify the path has been chosen by a player
+     */
+    void signalPathChosen(int cellId);
 }
