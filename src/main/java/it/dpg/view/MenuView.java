b72cf9bea8caac556fe294aa53b1bb74e2e9ddd1
Riccardo <riccard.squarcialupi@studio.unibo.it>
2020-04-16 16:14:27 +0200

    Added enum "Difficulty" for AI options

--- src/main/java/it/dpg/view/MenuView.java
+++ src/main/java/it/dpg/view/MenuView.java
@@ -2,6 +2,10 @@ package it.dpg.view;
 
 public interface MenuView {
 
+    enum Difficulty {
+        EASY,MEDIUM,HARD;
+    }
+
     void displayCredit();
     void displayOptions();
     void exitGUI();
