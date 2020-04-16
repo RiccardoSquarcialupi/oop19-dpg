710d0be17c4de937f3623ac4a7ac2403bd711a67
Riccardo <riccard.squarcialupi@studio.unibo.it>
2020-04-16 17:04:12 +0200

    add commment to methods

--- src/main/java/it/dpg/view/MenuView.java
+++ src/main/java/it/dpg/view/MenuView.java
@@ -1,14 +1,21 @@
 package it.dpg.view;
 
 public interface MenuView {
-
+    //difficulty for AI settings
     enum Difficulty {
         EASY,MEDIUM,HARD;
     }
 
+    //display credit in a new scene
     void displayCredit();
+
+    //display options in a new scene
     void displayOptions();
+
+    //exit the GUI
     void exitGUI();
+
+    //initialize the game with input parameters from option
     void startGame();
     
 }
