ab9c4f7d95b13217c86cb39b9cce72dddf2056fe
Miriana <miriana.ascenzo@studio.unibo.it>
2020-04-30 21:10:07 +0200

    fixed Grid interface

--- src/main/java/it/dpg/model/Grid.java
+++ src/main/java/it/dpg/model/Grid.java
@@ -1,7 +1,10 @@
 package it.dpg.model;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 public interface Grid {
     Cell getFirst();
     Cell getLast();
-    Cell getCellByID(int id);
+    Cell getCellByCoordinates(int X, int Y);
+    ImmutablePair<Integer, Integer> getCoordinatesByCell(Cell cell);
 }
