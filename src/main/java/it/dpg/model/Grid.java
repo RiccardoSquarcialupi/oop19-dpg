4a4d798888f8f1edcbdb51031944af0f82bdd96a
Miriana <miriana.ascenzo@studio.unibo.it>
2020-04-30 21:12:58 +0200

    fixed Grid interface

--- src/main/java/it/dpg/model/Grid.java
+++ src/main/java/it/dpg/model/Grid.java
@@ -6,5 +6,4 @@ public interface Grid {
     Cell getFirst();
     Cell getLast();
     Cell getCellByCoordinates(int X, int Y);
-    ImmutablePair<Integer, Integer> getCoordinatesByCell(Cell cell);
 }
