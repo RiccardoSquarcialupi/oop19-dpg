c97ac0243ccdfbeded22164094cf5f28ab6ed47c
Miriana <miriana.ascenzo@studio.unibo.it>
2020-05-08 17:21:57 +0200

    adjusted GridFactory interface and added GridType enum

--- src/main/java/it/dpg/model/GridFactory.java
+++ src/main/java/it/dpg/model/GridFactory.java
@@ -2,12 +2,11 @@ package it.dpg.model;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import java.io.File;
 import java.util.Map;
 
 public interface GridFactory {
 
-    void setJson(File jsonFile);
+    void setJson(GridType grid_type);
     void makeGrid();
     Map<Cell, ImmutablePair<Integer, Integer>> getGrid();
     void getFirst();
