0c084d1bd16b1e1b4ebf083cafb932bd3aee136b
Miriana <miriana.ascenzo@studio.unibo.it>
2020-05-08 18:02:55 +0200

    fixed Cell Implementation

--- src/main/java/it/dpg/model/CellImpl.java
+++ src/main/java/it/dpg/model/CellImpl.java
@@ -6,10 +6,17 @@ import java.util.Set;
 
 public class CellImpl implements Cell {
 
-    Set<Cell> nextCell;
-    boolean isAFork;
-    ImmutablePair<Integer, Integer> coordinates;
-    CellType type;
+    private final Set<Cell> nextCell;
+    private final boolean isAFork;
+    private final ImmutablePair<Integer, Integer> coordinates;
+    private final CellType type;
+
+    public CellImpl (final Set<Cell> nextCell, final boolean isAFork, final ImmutablePair<Integer, Integer> coordinates, final CellType type) {
+        this.nextCell = nextCell;
+        this.isAFork = isAFork;
+        this.coordinates = coordinates;
+        this.type = type;
+    }
 
     @Override
     public Boolean isAFork() {
