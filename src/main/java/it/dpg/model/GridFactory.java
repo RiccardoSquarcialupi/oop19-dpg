1756c5b2508b236fc87ad6e5447a26f2d1840c36
Miriana <miriana.ascenzo@studio.unibo.it>
2020-05-08 17:03:23 +0200

    adjusted GridFactory interface

--- src/main/java/it/dpg/model/GridFactory.java
+++ src/main/java/it/dpg/model/GridFactory.java
@@ -2,11 +2,12 @@ package it.dpg.model;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
+import java.io.File;
 import java.util.Map;
 
 public interface GridFactory {
 
-    void setJson();
+    void setJson(File jsonFile);
     void makeGrid();
     Map<Cell, ImmutablePair<Integer, Integer>> getGrid();
     void getFirst();
