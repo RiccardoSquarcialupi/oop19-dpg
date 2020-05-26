404a0a90aea12a4c0af72b3e6fd1002ffff7dd59
Davide Picchiotti <davide.picchiotti@studio.unibo.it>
2020-05-26 16:43:34 +0200

    Finished Character tests

--- src/test/java/it/dpg/characterTests/CharacterTest.java
+++ src/test/java/it/dpg/characterTests/CharacterTest.java
@@ -1,19 +1,54 @@
 package it.dpg.characterTests;
 
+import it.dpg.model.Cell;
+import it.dpg.model.CellImpl;
+import it.dpg.model.CellType;
 import it.dpg.model.Grid;
 import it.dpg.model.character.Character;
 import it.dpg.model.character.CharacterImpl;
 import it.dpg.model.character.Dice;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class CharacterTest {
 
     @Mock
     Grid grid;
+
+    ImmutablePair<Integer, Integer> startPos = new ImmutablePair<>(1, 1);
+    Cell startFork;
+
+    @BeforeEach
+    void setupMock() {
+        final Set<Cell> singleNext = new HashSet<>();
+        singleNext.add(new CellImpl(false, new ImmutablePair<>(2, 2), CellType.NORMAL));
+
+        final Cell startNoFork = new CellImpl(false, startPos, CellType.START);
+        startNoFork.setNext(singleNext);
+
+        final Set<Cell> forkNext = new HashSet<>();
+        forkNext.add(new CellImpl(false, new ImmutablePair<>(2, 2), CellType.NORMAL));
+        forkNext.add(new CellImpl(false, new ImmutablePair<>(3, 3), CellType.NORMAL));
+
+        startFork = new CellImpl(true, startPos, CellType.START);
+        startFork.setNext(forkNext);
+
+        setFirst(startNoFork);
+    }
 
     @Test
     public void startingValuesTest() {
@@ -35,26 +70,92 @@ public class CharacterTest {
         final Character c = new CharacterImpl(id, name, grid);
 
         for(int i = 0; i < 1000; i++) {
-            Assertions.assertTrue(c.throwDice() <= c.getDice().getFaces());
-            Assertions.assertTrue(c.throwDice() >= 1);
+            int n = c.throwDice();
+            Assertions.assertTrue(n <= c.getDice().getFaces() && n >= 1);
         }
 
         c.setDice(Dice.D4);
         for(int i = 0; i < 1000; i++) {
-            Assertions.assertTrue(c.throwDice() <= c.getDice().getFaces());
-            Assertions.assertTrue(c.throwDice() >= 1);
+            int n = c.throwDice();
+            Assertions.assertTrue(n <= c.getDice().getFaces() && n >= 1);
         }
 
         c.setDice(Dice.D8);
         for(int i = 0; i < 1000; i++) {
-            Assertions.assertTrue(c.throwDice() <= c.getDice().getFaces());
-            Assertions.assertTrue(c.throwDice() >= 1);
+            int n = c.throwDice();
+            Assertions.assertTrue(n <= c.getDice().getFaces() && n >= 1);
         }
 
         c.setDice(Dice.D10);
         for(int i = 0; i < 1000; i++) {
-            Assertions.assertTrue(c.throwDice() <= c.getDice().getFaces());
-            Assertions.assertTrue(c.throwDice() >= 1);
+            int n = c.throwDice();
+            Assertions.assertTrue(n <= c.getDice().getFaces() && n >= 1);
         }
+    }
+
+    @Test
+    public void stepForwardTestNoFork() {
+        final int id = 2;
+        final String name = "Lena";
+        final Character c = new CharacterImpl(id, name, grid);
+
+        Assertions.assertEquals(c.getPosition().left, startPos.left);
+        Assertions.assertEquals(c.getPosition().right, startPos.right);
+
+        Assertions.assertEquals(c.getAdjacentPositions().size(), 1);
+
+        c.throwDice();
+        c.stepForward();
+        Assertions.assertEquals(c.getPosition().left, 2);
+        Assertions.assertEquals(c.getPosition().right, 2);
+    }
+
+    @Test
+    public void stepForwardTestFork() {
+        setFirst(startFork);
+
+        final int id = 2;
+        final String name = "Lena";
+        final Character c = new CharacterImpl(id, name, grid);
+
+        Assertions.assertEquals(c.getPosition().left, startPos.left);
+        Assertions.assertEquals(c.getPosition().right, startPos.right);
+
+        Assertions.assertTrue(c.getAdjacentPositions().size() > 1);
+
+        c.throwDice();
+        Assertions.assertThrows(UnsupportedOperationException.class, c::stepForward);
+    }
+
+    @Test
+    public void stepToDirectionOk() {
+        final int id = 3;
+        final String name = "Faris";
+        Character c = new CharacterImpl(id, name, grid);
+
+        c.throwDice();
+        c.stepInDirection(new ImmutablePair<>(2, 2));
+    }
+
+    @Test
+    public void stepToDirectionKo() {
+        setFirst(startFork);
+
+        final int id = 3;
+        final String name = "Faris";
+        Character c = new CharacterImpl(id, name, grid);
+
+        c.throwDice();
+        c.stepInDirection(new ImmutablePair<>(3, 3));
+
+        c.throwDice();
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> c.stepInDirection(new ImmutablePair<>(4, 4))
+        );
+    }
+
+    private void setFirst(final Cell first) {
+        when(grid.getFirst()).thenReturn(first);
     }
 }
