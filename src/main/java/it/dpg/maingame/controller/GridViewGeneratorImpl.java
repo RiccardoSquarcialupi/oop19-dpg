6ef366803d4adf876853a9759c0003b939b66218
Miriana <miriana.ascenzo@studio.unibo.it>
2020-06-06 17:13:04 +0200

    fixed GridViewGenerator and its implementation

--- src/main/java/it/dpg/maingame/controller/GridViewGeneratorImpl.java
+++ src/main/java/it/dpg/maingame/controller/GridViewGeneratorImpl.java
@@ -3,13 +3,12 @@ package it.dpg.maingame.controller;
 import it.dpg.maingame.model.Grid;
 import it.dpg.maingame.view.GridView;
 import it.dpg.maingame.view.GridViewImpl;
-import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class GridViewGeneratorImpl implements GridViewGenerator {
 
     private final Grid grid;
-    public GridViewImpl view;
+    public GridView view;
 
     public GridViewGeneratorImpl (Grid grid) {
         this.grid = grid;
@@ -18,6 +17,6 @@ public class GridViewGeneratorImpl implements GridViewGenerator {
     @Override
     public void generate(Stage stage) {
         this.view = new GridViewImpl(grid);
-        view.startGeneration();
+        view.startGeneration(stage);
     }
 }
