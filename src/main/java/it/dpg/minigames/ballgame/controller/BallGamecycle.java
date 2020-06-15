a10fd6eeb0eef0ee9424f16815924c68d2f8e9de
Davide <davide.freddi3@studio.unibo.it>
2020-06-15 17:04:41 +0200

    interfaces documented

--- src/main/java/it/dpg/minigames/ballgame/controller/BallGamecycle.java
+++ src/main/java/it/dpg/minigames/ballgame/controller/BallGamecycle.java
@@ -3,11 +3,28 @@ package it.dpg.minigames.ballgame.controller;
 import it.dpg.minigames.base.controller.MinigameCycle;
 
 public interface BallGamecycle extends MinigameCycle {
+
+    /**
+     * signal the gamecycle the up input state
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void signalUpButton(boolean isPressed);
 
+    /**
+     * signal the gamecycle the down input state
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void signalDownButton(boolean isPressed);
 
+    /**
+     * signal the gamecycle the left input state
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void signalLeftButton(boolean isPressed);
 
+    /**
+     * signal the gamecycle the right input state
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void signalRightButton(boolean isPressed);
 }
--- src/main/java/it/dpg/minigames/ballgame/controller/BallMinigameObserver.java
+++ src/main/java/it/dpg/minigames/ballgame/controller/BallMinigameObserver.java
@@ -1,11 +1,28 @@
 package it.dpg.minigames.ballgame.controller;
 
 public interface BallMinigameObserver {
+
+    /**
+     * handle the up button
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void handleUpButton(boolean isPressed);
 
+    /**
+     * handle the down button
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void handleDownButton(boolean isPressed);
 
+    /**
+     * handle the left button
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void handleLeftButton(boolean isPressed);
 
+    /**
+     * handle the right button
+     * @param isPressed true if it's pressed, false if it's released
+     */
     void handleRightButton(boolean isPressed);
 }
