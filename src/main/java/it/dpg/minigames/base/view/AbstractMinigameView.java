1ad2a69375d0e022599534d6056b63e0731ad0f0
Davide Picchiotti <dpicchiotti@imolainformatica.it>
2020-06-17 15:34:13 +0200

    Fixed AbstractMinigameView to create Stage object in javafx thread

--- src/main/java/it/dpg/minigames/base/view/AbstractMinigameView.java
+++ src/main/java/it/dpg/minigames/base/view/AbstractMinigameView.java
@@ -6,23 +6,23 @@ import javafx.stage.Stage;
 
 public abstract class AbstractMinigameView implements MinigameView {
 
-    private Stage stage = new Stage();
+    private Stage stage;
 
     @Override
     public void setView() {
-        stage.setHeight(600d);
-        stage.setWidth(600d);
-        stage.setResizable(false);
-
-        stage.setScene(this.createScene());
-
-        Platform.runLater(() -> stage.show());
+        Platform.runLater(this::setViewUsingAppThread);
     }
-
-    public abstract Scene createScene();
 
     @Override
     public void closeView() {
         Platform.runLater(() -> stage.close());
     }
+
+    private void setViewUsingAppThread() {
+        stage = new Stage();
+        stage.setScene(createScene());
+        stage.show();
+    }
+
+    public abstract Scene createScene();
 }
