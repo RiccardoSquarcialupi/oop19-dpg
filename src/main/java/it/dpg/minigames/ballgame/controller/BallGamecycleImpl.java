f5362104f579963d1676cca09a18b7bdfab6c745
Davide <davide.freddi3@studio.unibo.it>
2020-06-17 11:33:39 +0200

    created class BallGamecycleImpl

---
+++ src/main/java/it/dpg/minigames/ballgame/controller/BallGamecycleImpl.java
@@ -0,0 +1,30 @@
+package it.dpg.minigames.ballgame.controller;
+
+import it.dpg.minigames.base.view.MinigameView;
+
+public class BallGamecycleImpl implements BallGamecycle{
+    @Override
+    public void signalUpButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public void signalDownButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public void signalLeftButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public void signalRightButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public int startCycle(MinigameView view) {
+        return 0;
+    }
+}
