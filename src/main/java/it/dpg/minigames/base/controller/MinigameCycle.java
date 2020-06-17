0b33134aebe9478f62365055e74f4d75363aa0eb
Davide Picchiotti <dpicchiotti@imolainformatica.it>
2020-06-17 15:52:47 +0200

    Fixed AbstractMinigame and MinigameCycle

--- src/main/java/it/dpg/minigames/base/controller/MinigameCycle.java
+++ src/main/java/it/dpg/minigames/base/controller/MinigameCycle.java
@@ -1,7 +1,5 @@
 package it.dpg.minigames.base.controller;
 
-import it.dpg.minigames.base.view.MinigameView;
-
 public interface MinigameCycle {
-    <T extends MinigameView> int startCycle(final T view);
+    int startCycle();
 }
