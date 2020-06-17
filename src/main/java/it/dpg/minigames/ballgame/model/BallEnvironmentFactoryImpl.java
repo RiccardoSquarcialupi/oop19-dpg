0e8ba53a644c2229820c8b201bb1883cb2f9ea45
Davide <davide.freddi3@studio.unibo.it>
2020-06-17 11:09:47 +0200

    factory for BallEnvironment implemented

---
+++ src/main/java/it/dpg/minigames/ballgame/model/BallEnvironmentFactoryImpl.java
@@ -0,0 +1,19 @@
+package it.dpg.minigames.ballgame.model;
+
+import it.dpg.minigames.ballgame.controller.BallMinigameLevel;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+
+public class BallEnvironmentFactoryImpl implements BallEnvironmentFactory {
+    @Override
+    public BallEnvironment createEnvironment(BallMinigameLevel level) {
+        Set<Boundary> temp = new HashSet<>();
+        temp.add(new HorizontalBoundary(0, 100, 100, CollisionType.BOUNCE));
+        temp.add(new HorizontalBoundary(0, 100, 0, CollisionType.BOUNCE));
+        temp.add(new VerticalBoundary(0, 0, 100, CollisionType.BOUNCE));
+        temp.add(new VerticalBoundary(100, 0, 100, CollisionType.BOUNCE));
+        return new BallEnvironmentImpl(50, 50, 5, temp, 30);
+    }
+}
