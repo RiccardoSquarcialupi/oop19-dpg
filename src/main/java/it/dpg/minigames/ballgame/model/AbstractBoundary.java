0142f4995ac8a0946fd79d8a27545100eda10347
Davide <davide.freddi3@studio.unibo.it>
2020-06-17 12:05:04 +0200

    implemented hash and equals on AbstractBoundary

--- src/main/java/it/dpg/minigames/ballgame/model/AbstractBoundary.java
+++ src/main/java/it/dpg/minigames/ballgame/model/AbstractBoundary.java
@@ -1,8 +1,7 @@
 package it.dpg.minigames.ballgame.model;
 
-
-import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.Objects;
 
 public abstract class AbstractBoundary implements Boundary {
     protected final Point2D start;
@@ -48,5 +47,19 @@ public abstract class AbstractBoundary implements Boundary {
     @Override
     public CollisionType getCollisionType() {
         return this.collisionType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractBoundary)) return false;
+        AbstractBoundary that = (AbstractBoundary) o;
+        return start.equals(that.start) &&
+                end.equals(that.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
     }
 }
