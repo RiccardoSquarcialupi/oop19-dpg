c6d95ff7ecf59a228bdce889927e559fcd99272a
Davide <davide.freddi3@studio.unibo.it>
2020-06-17 11:59:02 +0200

    created class BallGamecycleImpl

--- src/main/java/it/dpg/minigames/ballgame/model/BallEnvironmentFactoryImpl.java
+++ src/main/java/it/dpg/minigames/ballgame/model/BallEnvironmentFactoryImpl.java
@@ -7,6 +7,13 @@ import java.util.Set;
 import java.util.logging.Level;
 
 public class BallEnvironmentFactoryImpl implements BallEnvironmentFactory {
+
+    private final int expectedFPS;
+
+    BallEnvironmentFactoryImpl(int expectedFPS) {
+        this.expectedFPS = expectedFPS;
+    }
+
     @Override
     public BallEnvironment createEnvironment(BallMinigameLevel level) {
         Set<Boundary> temp = new HashSet<>();
@@ -14,6 +21,6 @@ public class BallEnvironmentFactoryImpl implements BallEnvironmentFactory {
         temp.add(new HorizontalBoundary(0, 100, 0, CollisionType.BOUNCE));
         temp.add(new VerticalBoundary(0, 0, 100, CollisionType.BOUNCE));
         temp.add(new VerticalBoundary(100, 0, 100, CollisionType.BOUNCE));
-        return new BallEnvironmentImpl(50, 50, 5, temp, 30);
+        return new BallEnvironmentImpl(50, 50, 5, temp, expectedFPS);
     }
 }
