9cadb5dfc0d13341a89b251bdc0b626d3b228e6c
Davide <davide.freddi3@studio.unibo.it>
2020-06-19 12:41:12 +0200

    MinigameType enum created

---
+++ src/main/java/it/dpg/minigames/MinigameType.java
@@ -0,0 +1,20 @@
+package it.dpg.minigames;
+
+import it.dpg.minigames.ballgame.controller.BallMinigame;
+import it.dpg.minigames.base.controller.Minigame;
+
+import java.util.function.Supplier;
+
+public enum MinigameType {
+    BALLGAME(BallMinigame::new);
+
+    private final Supplier<Minigame> implementationSupplier;
+
+    Minigame getMinigame() {
+        return implementationSupplier.get();
+    }
+
+    MinigameType(Supplier<Minigame> implementationSupplier) {
+        this.implementationSupplier = implementationSupplier;
+    }
+}
