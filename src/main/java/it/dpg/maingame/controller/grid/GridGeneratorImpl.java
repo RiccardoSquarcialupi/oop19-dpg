f20e7213d2d7958a27b12acc64173e0794f71539
Miriana <miriana.ascenzo@studio.unibo.it>
2020-06-23 17:38:29 +0200

    renamed GridViewGenerator in GridGenerator; removed unnecessary interfaces

--- src/main/java/it/dpg/maingame/controller/grid/GridGeneratorImpl.java
+++ src/main/java/it/dpg/maingame/controller/grid/GridGeneratorImpl.java
@@ -12,19 +12,18 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class GridViewGeneratorImpl implements GridViewGenerator {
+public class GridGeneratorImpl {
 
     public Map<Cell, Pair<Integer, Integer>> gridMap;
     public GridViewPlat view;
     private final GridType gridType;
     private final GameCycle gameCycle;
 
-    public GridViewGeneratorImpl(GridType type, GameCycle gameCycle) {
+    public GridGeneratorImpl(GridType type, GameCycle gameCycle) {
         this.gridType = type;
         this.gameCycle = gameCycle;
     }
 
-    @Override
     public Pair<Grid, GridView> generate() {
 
         /* The grid is initialized */
