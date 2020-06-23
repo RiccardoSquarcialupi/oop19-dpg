fda25d60c378424845d1d5050a9dfd784fc1e7a8
Davide Picchiotti <dpicchiotti@imolainformatica.it>
2020-06-23 11:28:42 +0200

    Fix to character tests, added JUMPGAME in MinigameTypeEnum

--- src/main/java/it/dpg/minigames/MinigameType.java
+++ src/main/java/it/dpg/minigames/MinigameType.java
@@ -2,13 +2,15 @@ package it.dpg.minigames;
 
 import it.dpg.minigames.ballgame.controller.BallMinigame;
 import it.dpg.minigames.base.controller.Minigame;
+import it.dpg.minigames.jumpgame.JumpMinigame;
 import it.dpg.minigames.punchygame.PunchyMinigame;
 
 import java.util.function.Supplier;
 
 public enum MinigameType {
     BALLGAME(BallMinigame::new),
-    PUNCHYGAME(PunchyMinigame::new);
+    PUNCHYGAME(PunchyMinigame::new),
+    JUMPGAME(JumpMinigame::new);
 
     private final Supplier<Minigame> implementationSupplier;
 
